a9b36b0d47a9607282449a0cbf93583223198d63
Amit Sule <amit.sule@gmail.com>
2019-02-23 17:13:00 +0000

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/com/cognitech/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package com.cognitech.sfgpetclinic.services;/*
+ * Created by User on 23/02/2019
+ */
+
+import com.cognitech.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>
+{
+}
