50c4108f63c2260fb9ef29e40faa565b8850817c
Amit Sule <amit.sule@gmail.com>
2019-02-23 17:01:27 +0000

    Added Speciality entity. Updated Vet with Speciality relationship. Closes #27

---
+++ pet-clinic-data/src/main/java/com/cognitech/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,20 @@
+/*
+ * Created by User on 23/02/2019
+ */
+
+package com.cognitech.sfgpetclinic.model;
+
+public class Speciality extends BaseEntity
+{
+    private String description;
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+}
