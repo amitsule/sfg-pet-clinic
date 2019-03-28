89f87f1e5a4b8166c57e6a52b88a9d1a869e3835
Amit Sule <amit.sule@gmail.com>
2019-03-28 17:24:30 +0000

    Updated vets with JPA entity mappings. Closes #37

--- pet-clinic-data/src/main/java/com/cognitech/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/cognitech/sfgpetclinic/model/Speciality.java
@@ -4,8 +4,15 @@
 
 package com.cognitech.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity
 {
+    @Column(name = "description")
     private String description;
 
     public String getDescription()
