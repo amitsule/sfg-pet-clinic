e76eb4b53b0c869e4846b76044e466a7339e56f1
Amit Sule <amit.sule@gmail.com>
2019-05-22 08:07:46 +0100

    removed cognitech code

--- pet-clinic-data/src/main/java/com/cognitech/sfgpetclinic/services/SpecialityService.java
+++
@@ -1,9 +0,0 @@
-package com.cognitech.sfgpetclinic.services;/*
- * Created by User on 23/02/2019
- */
-
-import com.cognitech.sfgpetclinic.model.Speciality;
-
-public interface SpecialityService extends CrudService<Speciality, Long>
-{
-}
